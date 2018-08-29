795fdb62fbbdc8997556e18366102b0ff8cf7db4
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-08-29 19:14:51 +0500

    1

--- index.php
+++ index.php
@@ -21,29 +21,29 @@
 		<div class="row">
 			<div class="col-xl-12">
 				<div class="wrapper">
-
-<div class="content" id="content1">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae erat vel purus pharetra viverra vel ac risus. Vivamus aliquet nunc pulvinar magna dignissim semper. Mauris eget tellus feugiat, suscipit ex non, suscipit nibh. Sed eu consectetur dui. Sed arcu neque, blandit at neque ac, auctor malesuada velit. Cras interdum, urna id mattis iaculis, quam magna porta neque, sit amet mollis lectus nibh at lacus. Morbi iaculis cursus iaculis. Proin nulla ex, consectetur ut finibus non, pellentesque et ipsum. Proin commodo arcu at nunc commodo, quis consequat ligula sollicitudin. Sed interdum neque nec elit dapibus, non pellentesque turpis maximus. Duis tristique erat vitae dui ultrices efficitur. Morbi tempus malesuada arcu ut aliquam. Aliquam ullamcorper ornare ipsum ut malesuada. Sed feugiat placerat sapien at dictum.</p>
-</div>
-<div class="content" id="content2">
-    <p>Nam malesuada lorem non dui porta, sed tempus nulla porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer ac lacus pulvinar, laoreet turpis ut, consequat est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut vitae ligula non est mollis faucibus sit amet sed ipsum. Sed rutrum, quam id faucibus lobortis, tellus nulla fermentum velit, in porta diam lorem sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin laoreet mauris ut mauris maximus, eget interdum neque mollis. Praesent eget sem pulvinar, volutpat ex condimentum, elementum arcu. Vivamus efficitur est ipsum, a tempor purus gravida id. Duis fermentum, leo vel gravida imperdiet, ligula magna cursus ante, vel lacinia erat sem nec ante. Proin id purus eget neque finibus rhoncus. Quisque ut velit nisl. Ut non faucibus sapien, et laoreet risus.</p>
-</div>
-<div class="content" id="content3" data-stellar-ratio="2">
-    <p>Sed a porta lorem, id vulputate mauris. Sed non erat sed velit vestibulum suscipit nec non augue. Cras vitae dolor ac lorem dapibus molestie. Proin dapibus feugiat sollicitudin. Etiam imperdiet tortor at lorem auctor rutrum nec dictum justo. Cras molestie in elit sed semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla et sodales nulla. Vestibulum quis aliquam elit. Nullam placerat orci accumsan elit laoreet, sed sollicitudin sapien dignissim. Pellentesque molestie ut lacus eget vehicula. Sed felis odio, feugiat in euismod ac, commodo eget turpis. Sed non ultrices ante.</p>
-</div>
-<div class="content" id="content4" data-stellar-background-ratio="1.5">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae erat vel purus pharetra viverra vel ac risus. Vivamus aliquet nunc pulvinar magna dignissim semper. Mauris eget tellus feugiat, suscipit ex non, suscipit nibh. Sed eu consectetur dui. Sed arcu neque, blandit at neque ac, auctor malesuada velit. Cras interdum, urna id mattis iaculis, quam magna porta neque, sit amet mollis lectus nibh at lacus. Morbi iaculis cursus iaculis. Proin nulla ex, consectetur ut finibus non, pellentesque et ipsum. Proin commodo arcu at nunc commodo, quis consequat ligula sollicitudin. Sed interdum neque nec elit dapibus, non pellentesque turpis maximus. Duis tristique erat vitae dui ultrices efficitur. Morbi tempus malesuada arcu ut aliquam. Aliquam ullamcorper ornare ipsum ut malesuada. Sed feugiat placerat sapien at dictum.</p>
-</div>
-<div class="content" id="content5" data-stellar-background-ratio="0.5">
-    <p>Nam malesuada lorem non dui porta, sed tempus nulla porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer ac lacus pulvinar, laoreet turpis ut, consequat est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut vitae ligula non est mollis faucibus sit amet sed ipsum. Sed rutrum, quam id faucibus lobortis, tellus nulla fermentum velit, in porta diam lorem sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin laoreet mauris ut mauris maximus, eget interdum neque mollis. Praesent eget sem pulvinar, volutpat ex condimentum, elementum arcu. Vivamus efficitur est ipsum, a tempor purus gravida id. Duis fermentum, leo vel gravida imperdiet, ligula magna cursus ante, vel lacinia erat sem nec ante. Proin id purus eget neque finibus rhoncus. Quisque ut velit nisl. Ut non faucibus sapien, et laoreet risus.</p>
-</div>
-<div class="content" id="content6" data-stellar-background-ratio="0.5">
-    <p>Sed a porta lorem, id vulputate mauris. Sed non erat sed velit vestibulum suscipit nec non augue. Cras vitae dolor ac lorem dapibus molestie. Proin dapibus feugiat sollicitudin. Etiam imperdiet tortor at lorem auctor rutrum nec dictum justo. Cras molestie in elit sed semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla et sodales nulla. Vestibulum quis aliquam elit. Nullam placerat orci accumsan elit laoreet, sed sollicitudin sapien dignissim. Pellentesque molestie ut lacus eget vehicula. Sed felis odio, feugiat in euismod ac, commodo eget turpis. Sed non ultrices ante.</p>
-</div>
-
-
-
-
+					<div id="parallax-bg3">
+					 <img id="bg3-1" src="img/ico-1.png" width="529" height="757" alt="Montgolfier hot air balloon"/>
+					 <img id="bg3-2" src="img/ico-2.png" width="529" height="757" alt="Frameless parachute"/>
+					 <img id="bg3-3" src="img/ico-3.png" width="446" height="713" alt="Blanchard's air balloon"/>
+					 <img id="bg3-4" src="img/ico-4.png" width="1104" height="684" alt="Landscape with trees and cows"/>
+					 </div>
+					 
+					 <!-- Второй слой -->
+					 <div id="parallax-bg2">
+					 <img id="bg2-1" src="img/ico-5.png" alt="cloud"/>
+					 <img id="bg2-2" src="img/ico-1.png" alt="cloud"/>
+					 <img id="bg2-3" src="img/ico-2.png" alt="cloud"/>
+					 <img id="bg2-4" src="img/ico-3.png" alt="cloud"/>
+					 <img id="bg2-5" src="img/ico-4.png" alt="cloud"/>
+					 </div>
+					 
+					 <!-- Третий слой -->
+					 <div id="parallax-bg1">
+					 <img id="bg1-1" src="img/ico-5.png" alt="cloud"/>
+					 <img id="bg1-2" src="img/ico-1.png" alt="cloud"/>
+					 <img id="bg1-3" src="img/ico-2.png" alt="cloud"/>
+					 <img id="bg1-4" src="img/ico-3.png" alt="cloud"/>
+					 </div>
 				</div>
 			</div>
 		</div>
@@ -51,7 +51,12 @@
 </main>
 
 
-
+<section id="home" data-type="background" data-speed="10" class="pages">
+         <article>Простая техника Parallax Scrolling</article>
+</section>
+<section id="about" data-type="background" data-speed="10" class="pages">
+         <article>Простая техника Parallax Scrolling</article>
+</section>
 
 
 
@@ -60,6 +65,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
 <script src="/jquery.stellar.js"></script>
+<script src="/jquery.plugin.js"></script>
 <script src="/main.js"></script>
 </body>
 </html>
